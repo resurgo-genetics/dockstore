fbb03717efbaa2118dd91e084d3e1e10251a5d6b
garyluu <garylau.work@gmail.com>
2017-05-18 11:28:51 -0400

    Initial attempt at having a default test parameter file location

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/AbstractImageRegistry.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/AbstractImageRegistry.java
@@ -25,6 +25,7 @@ import java.util.Set;
 import io.dockstore.client.cli.nested.AbstractEntryClient;
 import io.dockstore.common.Registry;
 import io.dockstore.webservice.core.Entry;
+import io.dockstore.webservice.core.SourceFile;
 import io.dockstore.webservice.core.Tag;
 import io.dockstore.webservice.core.Token;
 import io.dockstore.webservice.core.Tool;
@@ -78,6 +79,7 @@ public abstract class AbstractImageRegistry {
 
     /**
      * Returns the registry associated with the current class
+     *
      * @return registry associated with class
      */
     public abstract Registry getRegistry();
@@ -151,8 +153,8 @@ public abstract class AbstractImageRegistry {
         }
 
         // If exists, check conditions to see if it should be changed to auto (in sync with quay tags and git repo)
-        if (tool.getMode() == ToolMode.MANUAL_IMAGE_PATH && duplicatePath != null  && tool.getRegistry().name().equals(
-                Registry.QUAY_IO.name()) && duplicatePath.getGitUrl().equals(tool.getGitUrl())) {
+        if (tool.getMode() == ToolMode.MANUAL_IMAGE_PATH && duplicatePath != null && tool.getRegistry().name()
+                .equals(Registry.QUAY_IO.name()) && duplicatePath.getGitUrl().equals(tool.getGitUrl())) {
             tool.setMode(duplicatePath.getMode());
         }
 
@@ -248,6 +250,16 @@ public abstract class AbstractImageRegistry {
                             oldTag.setCwlPath(tool.getDefaultCwlPath());
                             oldTag.setWdlPath(tool.getDefaultWdlPath());
                             oldTag.setDockerfilePath(tool.getDefaultDockerfilePath());
+                            oldTag.setCwlTestFile(tool.getDefaultTestCwlParameterFile());
+                            oldTag.setWdlTestFile(tool.getDefaultTestWdlParameterFile());
+                            SourceFile sourcefile = new SourceFile();
+                            sourcefile.setPath(tool.getDefaultTestCwlParameterFile());
+                            sourcefile.setType(SourceFile.FileType.CWL_TEST_JSON);
+                            SourceFile sourcefile2 = new SourceFile();
+                            sourcefile2.setPath(tool.getDefaultTestWdlParameterFile());
+                            sourcefile2.setType(SourceFile.FileType.WDL_TEST_JSON);
+                            oldTag.getSourceFiles().add(sourcefile);
+                            oldTag.getSourceFiles().add(sourcefile2);
                         }
 
                         break;
@@ -259,6 +271,14 @@ public abstract class AbstractImageRegistry {
                     // this could result in the same tag being added to multiple containers with the same path, need to clone
                     Tag clonedTag = new Tag();
                     clonedTag.clone(newTag);
+                    SourceFile sourcefile = new SourceFile();
+                    sourcefile.setPath(tool.getDefaultTestCwlParameterFile());
+                    sourcefile.setType(SourceFile.FileType.CWL_TEST_JSON);
+                    SourceFile sourcefile2 = new SourceFile();
+                    sourcefile2.setPath(tool.getDefaultTestWdlParameterFile());
+                    sourcefile2.setType(SourceFile.FileType.WDL_TEST_JSON);
+                    clonedTag.getSourceFiles().add(sourcefile);
+                    clonedTag.getSourceFiles().add(sourcefile2);
                     existingTags.add(clonedTag);
                 }
 
@@ -322,7 +342,6 @@ public abstract class AbstractImageRegistry {
                 LOG.info(githubToken.getUsername() + " : Parsing WDL...");
                 sourceCodeRepo.updateEntryMetadata(tool, AbstractEntryClient.Type.WDL);
             }
-
         }
         toolDAO.create(tool);
 
